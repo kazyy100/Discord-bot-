2ea5f5f72e0ca81aa88878744ca5e36d79762b0e
Alzheimer <khalisamp0@gmail.com>
2026-05-09 11:29:44 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,35 @@
 import discord
-import asyncio
+from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Kita telah masuk sebagai {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
 
-    if message.content.startswith('$halo'):
-        await message.channel.send("Hi!")
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
 
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
+@bot.command()
+async def genpass(ctx, pass_length: int):
+    from botlogic import gen_pass
+    password = gen_pass(pass_length)
+    await ctx.send(f"Generated password: {password}")
 
-    elif message.content.startswith('$broadcast'):
-        for channel in message.guild.text_channels:
-            try:
-                await channel.send("sigma")
-                await asyncio.sleep(1)  # delay biar aman
-            except:
-                pass
+@bot.command()
+async def animequote(ctx, anime: str):
+    from botlogic import gen_anime_quote
+    quote = gen_anime_quote(anime.upper())
+    await ctx.send(quote)
 
-client.run("YOUR_TOKEN")
+
+
+bot.run("MTQ5OTk4MDY4OTM1NTk2NDU1Ng.GUQwN1.porGeFu2-qIJsZku_h8uP7kvWREDBA5GixeRDE")
